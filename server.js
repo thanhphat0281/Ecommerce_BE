aae3af8e14c670b7d60e9f45f99e9a797d57a0ee
ThanhPhat <thanhphat0281@gmail.com>
2022-11-16 18:17:48 +0700

    update

---
+++ server.js
@@ -0,0 +1,96 @@
+const bodyParser = require("body-parser");
+
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+
+const dbConfig = require("./app/config/db.config");
+
+const app = express();
+
+var corsOptions = {
+  origin: ["http://localhost:4200"],
+  credentials: true
+}
+app.use(express.json());
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/json
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "bezkoder-session",
+    secret: "COOKIE_SECRET", // should use as secret environment variable
+    httpOnly: true
+  })
+);
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb+srv://thanhphat0281:Thanhphat6181@cluster0.he52opv.mongodb.net/Dbtest`)
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// routes
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
